84bf4416d591d351e77214348d92ae45874cdc43
noobandy <noobandy1364@gmail.com>
2014-06-17 15:18:01 +0530

    Remove item on click and Clear list on selection reset form options

--- ListViewSample/src/in/anandm/android/apps/listviewsample/MainActivity.java
+++ ListViewSample/src/in/anandm/android/apps/listviewsample/MainActivity.java
@@ -1,9 +1,14 @@
 package in.anandm.android.apps.listviewsample;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,8 +18,8 @@ public class MainActivity extends ListActivity {
 	private Button addPersonButton;
 	private EditText personNameText;
 	private static final String[] PERSONS = { "Amit Tiwari", "Anand Mohan",
-		"Vijay Kumar Chauhan", "Nilesh Patil", "Onkar Sutar",
-		"Sunil Rajak", "Mr. Bean", "Ninza Coder", "Android", "QWERTY" };
+			"Vijay Kumar Chauhan", "Nilesh Patil", "Onkar Sutar",
+			"Sunil Rajak", "Mr. Bean", "Ninza Coder", "Android", "QWERTY" };
 
 	private ArrayAdapter<String> adapter = null;
 
@@ -25,9 +30,26 @@ public class MainActivity extends ListActivity {
 
 		initializeUI();
 
-		adapter = new MyArrayAdapter(this,PERSONS);
+		ArrayList<String> entries = new ArrayList<String>(
+				Arrays.asList(PERSONS));
+
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, entries);
 
 		setListAdapter(adapter);
+
+		getListView().setOnItemClickListener(
+				new AdapterView.OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view,
+							int position, long id) {
+						//remove item
+						adapter.remove(adapter.getItem(position));
+						//notify data set changed
+						adapter.notifyDataSetChanged();
+					}
+				});
 
 	}
 
@@ -36,6 +58,22 @@ public class MainActivity extends ListActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_reset:
+			//clear data
+			adapter.clear();
+			//notify data set changed
+			adapter.notifyDataSetChanged();
+			break;
+
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	private void initializeUI() {
@@ -58,10 +96,12 @@ public class MainActivity extends ListActivity {
 
 		if ("".equals(personName.toString().trim())) {
 			personNameText
-			.setError("Person name can not be empty or whitspace");
+					.setError("Person name can not be empty or whitspace");
 		} else {
 			// add person
 			adapter.add(personName);
+			// clear text field
+			personNameText.setText("");
 			// notify data has changed
 			adapter.notifyDataSetChanged();
 		}
